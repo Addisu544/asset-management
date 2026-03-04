6e206f7e3abe365a7f6627fc9f0d66a672f69732
Addisu Agerie <addisuagerie544@gmail.com>
2026-03-04 10:56:49 +0500

    feat(api): configure aaxios with base url and add endpoints list (8.1)

---
+++ asset-management-frontend/src/api/endpoints.ts
@@ -0,0 +1,12 @@
+export const ENDPOINTS = {
+  AUTH: {
+    LOGIN: "/Auth/login",
+    PROFILE: "/Auth/my-profile",
+  },
+  EMPLOYEES: "/Employees",
+  PRODUCTS: "/Products",
+  TRANSACTIONS: "/Transactions",
+  ASSET_GROUPS: "/AssetGroups",
+  ASSET_TYPES: "/AssetTypes",
+  DEPARTMENTS: "/Departments",
+};
