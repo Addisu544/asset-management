da86ecaf3f8833272e92a91bf9e36a0b7fd4b915
Addisu Agerie <addisuagerie544@gmail.com>
2026-03-19 13:05:02 +0500

    feat(product): add image of product on my properties

--- asset-management-frontend/src/pages/user/MyPropertiesPage.tsx
+++ asset-management-frontend/src/pages/user/MyPropertiesPage.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
+import { Box, Button } from "@mui/material";
 import DataTable from "../../components/common/DataTable";
 import StatusBadge from "../../components/common/StatusBadge";
 import { userService } from "../../services/userService";
+import ProductDetailsDialog from "../products/ProductDetailsDialog";
+
 console.log("reached above");
 
 const MyPropertiesPage = () => {
   console.log("reached");
-
+  const [openDetails, setOpenDetails] = useState(false);
+  const [selected, setSelected] = useState<any>(null);
   const [products, setProducts] = useState([]);
   console.log("reached");
 
@@ -39,9 +43,41 @@ const MyPropertiesPage = () => {
       width: 120,
       renderCell: (params: any) => <StatusBadge status={params.value} />,
     },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 260,
+      renderCell: (params: any) => {
+        const row = params.row;
+
+        return (
+          <Box>
+            <Button
+              size="small"
+              onClick={() => {
+                setSelected(row);
+                setOpenDetails(true);
+              }}
+            >
+              View
+            </Button>
+          </Box>
+        );
+      },
+    },
   ];
 
-  return <DataTable rows={products} columns={columns} />;
+  return (
+    <>
+      {" "}
+      <DataTable rows={products} columns={columns} />;
+      <ProductDetailsDialog
+        open={openDetails}
+        onClose={() => setOpenDetails(false)}
+        product={selected}
+      />
+    </>
+  );
 };
 
 export default MyPropertiesPage;
